6d9aba3866e8a9b2c2b4e6252e931e2190e1b123
kamdemcorine <127659943+kamdemcorine@users.noreply.github.com>
2024-11-19 12:19:45 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 #import matplotlib.pyplot as plt
 
 
 data = pd.read_csv('Iris.csv', delimiter = ';')
 st.title('Mon premier dashboard avec Streamlit')
 #st.table(data)
+fig = px.box(data, x='SepalLength', y='Species')
 
-st.map(data)
+st.plotly_chart(fig, use_container_width=True)
