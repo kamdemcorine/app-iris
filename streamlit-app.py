8c6642a113edb72a429192787e4fd6c7d1f55d06
kamdemcorine <127659943+kamdemcorine@users.noreply.github.com>
2024-11-19 12:23:09 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,12 +1,16 @@
+!pip  install streamlit altair
+
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import altair as alt
 #import matplotlib.pyplot as plt
 
 
 data = pd.read_csv('Iris.csv', delimiter = ';')
 st.title('Mon premier dashboard avec Streamlit')
 #st.table(data)
-fig = px.box(data, x='SepalLength', y='Species')
 
-st.plotly_chart(fig, use_container_width=True)
+# Créer un chart Altair 
+chart = alt.Chart(data).mark_bar().encode( x='SepaLength', y='Series') 
+# Afficher le chart sur Streamlit 
+st.altair_chart(chart, use_container_width=True)
