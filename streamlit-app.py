795c85e5ed9ab784a5341125001829345bb63cc7
kamdemcorine <127659943+kamdemcorine@users.noreply.github.com>
2024-11-19 12:08:58 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -7,11 +7,4 @@ data = pd.read_csv('Iris.csv', delimiter = ';')
 st.title('Mon premier dashboard avec Streamlit')
 #st.table(data)
 
-# Tab 2: Visualization
-st.header("Visualization")
-# Add your visualization code here (e.g., countplot, histogram)
-st.subheader("Distribution of Species")
-fig1 = plt.figure()
-sns.countplot(x="Species", data=df)
-plt.title('distribution des especes d\'iris')
-st.pyplot(fig1)
+st.map(data)
